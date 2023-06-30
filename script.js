d31f81db605a632604a266aef040df5283b74d45
eric-merritt <ericm2009@gmail.com>
2023-06-30 09:35:26 -0600

    Having working grid with trails     working on button now

--- script.js
+++ script.js
@@ -1,39 +1,226 @@
+
 const divContainer = document.getElementById('container');
 
-divContainer.style.flexDirection = "row";
 
+divContainer.style.width = "900px";
+divContainer.style.height = "900px";
+divContainer.style.display = "flex";
+divContainer.style.flex = "1 1 0";
+divContainer.style.flexDirection = "column";
+divContainer.style.border = "2px";
+divContainer.style.borderStyle = "solid";
+divContainer.style.alignContent = 'center';
+divContainer.style.margin = "auto";
 
-for (x = 1; x <= 256; x++){ // Loop for creating 256 divs. Starting with 1 in case I need to use every 16 divs to create new rows 
-
+const btnCont = document.createElement('div');
+    btnCont.style.width = "900px";
+    btnCont.style.alignContent = 'center';
+    btnCont.style.margin = 'auto';
+    btnCont.style.padding = '10px';
+    btnCont.style.display = 'flex';
+    btnCont.style.flexDirection = "column";
     
-    const gridDivs = document.createElement('div');
+const gridSizer = document.createElement('button');
+    gridSizer.innerText = "Resize Grid";
+    gridSizer.style.fontSize = "20px";
+    gridSizer.style.color = "red";
+    gridSizer.style.padding = '8px';
+    gridSizer.style.borderStyle = "ridge";
+    gridSizer.style.borderWidth = "5px"
+    gridSizer.style.backgroundColor = "black";  
+  
 
 
-    gridDivs.className = `${x}`; // iterating each div with a different class so they can be ordered
+btnCont.appendChild(gridSizer);
 
-    
-    if (x != 16 && x % 16 != 0){     // special borders for divs that will be on ends so that they don't double with container border
-        gridDivs.style.width = '50px';
-        gridDivs.style.height = '50px';
-        gridDivs.style.borderBottom = '2px';
-        gridDivs.style.borderBottomStyle = 'solid';
-        gridDivs.style.borderRight = '2px';
-        gridDivs.style.borderRightStyle = 'solid';
-        gridDivs.style.borderColor = 'black';
-        gridDivs.style.float = "left"; // allows divs to be in row
-    } else {
-        gridDivs.style.width = '50px';
-        gridDivs.style.height = '50px';
-        gridDivs.style.borderBottom = '2px';
-        gridDivs.style.borderBottomStyle = 'solid';
+document.body.appendChild(btnCont);
+
+document.body.style.backgroundColor = "red";
+document.body.style.paddingTop = "auto";
+
+
+
+gridSizer.addEventListener('mouseover', function(){
+    const orgColor = gridSizer.style.color;
+
+    if (orgColor === 'red'){
+        gridSizer.style.color = "green";
+        gridSizer.style.backgroundColor = "darkgrey";
+    } 
+})
+
+    gridSizer.addEventListener('mouseout', (event) => {
+
+    event.target.style.color = "red";
+        event.target.style.backgroundColor = "black";
     }
+    )
+
+  
+            for (y = 1; y <= 16; y++){   
+
+                             
+          
+
     
+                const rowDivs = document.createElement('div');
+            
+                    rowDivs.className = "row"+ `${y}`;
+            
+                    rowDivs.style.clear = "both";
+            
+                    rowDivs.style.flex = "1 1 0";
+            
+                    rowDivs.style.display = "flex";
+            
+            
+                for (x = 1; x <= 16; x++){
+            
+                const gridDivs = document.createElement('div');
+            
+                    gridDivs.className = "grid";
+            
+            
+                
+                rowDivs.appendChild(gridDivs);
+            
+                    gridDivs.style.backgroundColor = "white"; //setting original background color
+            
+            
+            gridDivs.addEventListener('mouseover', function(){ //function to add color change on mouseover
+                const orgColor = gridDivs.style.backgroundColor;
+            
+                if (orgColor === 'white'){
+                    gridDivs.style.backgroundColor = 'black';
+                }
+            });
+            
+            
+            if (y < 16){
+            
+                if (x < 16){     
+            
+                    gridDivs.style.flexGrow = "1";
+                    gridDivs.style.borderBottom = '2px';
+                    gridDivs.style.borderBottomStyle = 'solid';
+                    gridDivs.style.borderRight = '2px';
+                    gridDivs.style.borderRightStyle = 'solid';
+                    gridDivs.style.borderColor = 'black';
+                    gridDivs.style.float = "left"; // allows divs to be in row
+            
+                    
+                } else { 
+                                        // special borders for divs that will be on ends so that they don't double with container border
+                    gridDivs.style.flex = "1 1 0"; 
+                    gridDivs.style.borderBottom = '2px';
+                    gridDivs.style.borderBottomStyle = 'solid';
+                    gridDivs.style.float = "left";
+            
+                } 
+            
+            } else {
+            
+                if (x < 16){     
+            
+                    gridDivs.style.flex = "1 1 0"; 
+                    gridDivs.style.borderRight = '2px';
+                    gridDivs.style.borderRightStyle = 'solid';
+                    gridDivs.style.borderColor = 'black';
+                    gridDivs.style.float = "left"; // allows divs to be in row
+            
+                    
+                } else { 
+                                        // special borders for divs that will be on ends so that they don't double with container border
+                    gridDivs.style.flex = "1 1 0";  
+                    gridDivs.style.float = "left";
+            
+                } 
+            
+                   
+            
+            
+                
+                 } 
+         
+        } document.getElementById('container').appendChild(rowDivs);
+    }
+        
+        
+             
+              
+
+      
+    
+            
+            
+            
+     
+        
+
+
+
+   
+  
+
+        
+            
+               
+                
+
+            
+            
+
+                    
+
+        
+
+    
+          
+           
+
+
+
+
+
+
+        
     
         
-document.getElementById('container').appendChild(gridDivs);
 
-    
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+ 
+
+        
+
+
+
+
+
+
+ 
+        
+
+
+
+
+
 
 
 
