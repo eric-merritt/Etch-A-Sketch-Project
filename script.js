e41769609f3f2fcc3131edf0b17ad4cac1098024
eric-merritt <ericm2009@gmail.com>
2023-07-02 15:58:58 -0600

    Changed the outerdiv class to fit more flexcnt     added rgb function     user input control working for grid resize

--- script.js
+++ script.js
@@ -1,104 +1,291 @@
+document.body.onload = createGrid;
 
-const divContainer = document.getElementById('container');
+const destDiv = document.getElementById('destDiv');
+    destDiv.style.display = 'flex';
+    destDiv.style.flexDirection = 'row';
+    destDiv.style.justifyContent = 'space-around';
+
+const colorBtnDiv = document.createElement('form');
+    colorBtnDiv.style.display = 'flex';
+    colorBtnDiv.style.flexDirection = 'column';
+    colorBtnDiv.style.justifyContent = 'flex-end';
+
+const colorBtn = document.createElement('button');
+    colorBtn.innerText = 'Random Color'
+    colorBtn.style.borderRadius = '100%';
+    colorBtn.style.fontWeight = '700';
+    colorBtn.style.fontSize = '25px';
+    colorBtn.style.height = '150px';
+    colorBtn.style.width = '150px';
+
+colorBtnDiv.appendChild(colorBtn);
+
+const centerDiv = document.createElement('div');
+    centerDiv.style.display = 'flex';
+    centerDiv.style.flexDirection = 'column';
 
 
-divContainer.style.width = "900px";
-divContainer.style.height = "900px";
+const divContainer = document.createElement('div');
+    divContainer.className = "container";
+    divContainer.setAttribute('id','container');
+
+
+divContainer.style.width = "800px";
+divContainer.style.height = "800px";
 divContainer.style.display = "flex";
-divContainer.style.flex = "1 1 0";
 divContainer.style.flexDirection = "column";
-divContainer.style.border = "2px";
-divContainer.style.borderStyle = "solid";
+divContainer.style.border = "5px";
+divContainer.style.borderStyle = "inset";
 divContainer.style.alignContent = 'center';
-divContainer.style.margin = "auto";
+divContainer.style.marginBottom = '60px';
+
+
 
 const btnCont = document.createElement('div');
-    btnCont.style.width = "900px";
     btnCont.style.alignContent = 'center';
-    btnCont.style.margin = 'auto';
-    btnCont.style.padding = '10px';
+    btnCont.style.width = '100%'
+    btnCont.style.paddingTop = '5px';
+    btnCont.style.paddingBottom = '10px';
     btnCont.style.display = 'flex';
-    btnCont.style.flexDirection = "column";
+    btnCont.style.flexDirection = 'row';
+    btnCont.style.justifyContent = 'center';
+    btnCont.style.alignContent = 'center';
     
-const gridSizer = document.createElement('button');
-    gridSizer.innerText = "Resize Grid";
-    gridSizer.style.fontSize = "20px";
-    gridSizer.style.color = "red";
-    gridSizer.style.padding = '8px';
-    gridSizer.style.borderStyle = "ridge";
-    gridSizer.style.borderWidth = "5px"
-    gridSizer.style.backgroundColor = "black";  
-  
+   
+const resizeBtn = document.createElement('button');
+    resizeBtn.innerText = "Resize Grid";
+    resizeBtn.style.width = "200px";
+    resizeBtn.style.fontSize = "20px";
+    resizeBtn.style.fontWeight = '700';
+    resizeBtn.style.color = "red";
+    resizeBtn.style.padding = '8px';
+    resizeBtn.style.borderStyle = "ridge";
+    resizeBtn.style.borderWidth = "5px"
+    resizeBtn.style.backgroundColor = "black";
+    
+
+btnCont.appendChild(resizeBtn);
+centerDiv.appendChild(btnCont);
+centerDiv.appendChild(divContainer);
+    
+    
+const resetBtnDiv = document.createElement('div');
+    resetBtnDiv.style.display = 'flex';
+    resetBtnDiv.style.flexDirection = 'column';
+    resetBtnDiv.style.justifyContent = 'flex-end';
 
 
-btnCont.appendChild(gridSizer);
+const resetBtn = document.createElement('button');
+    resetBtn.textContent = 'Reset';
+    resetBtn.style.borderRadius = '100%';
+    resetBtn.style.fontWeight = '700';
+    resetBtn.style.fontSize = '25px';
+    resetBtn.style.height = '150px';
+    resetBtn.style.width = '150px';
 
-document.body.appendChild(btnCont);
+
+resetBtnDiv.appendChild(resetBtn);
+
+
+
+destDiv.appendChild(colorBtnDiv);
+destDiv.appendChild(centerDiv);
+destDiv.appendChild(resetBtnDiv);
+
+                 
+                 
+document.body.appendChild(destDiv);
+
 
 document.body.style.backgroundColor = "red";
 document.body.style.paddingTop = "auto";
 
 
-
-gridSizer.addEventListener('mouseover', function(){
-    const orgColor = gridSizer.style.color;
+resizeBtn.addEventListener('mouseover', function(){
+    const orgColor = resizeBtn.style.color;
 
     if (orgColor === 'red'){
-        gridSizer.style.color = "green";
-        gridSizer.style.backgroundColor = "darkgrey";
+        resizeBtn.style.color = "green";
+        resizeBtn.style.backgroundColor = "darkgrey";
     } 
 })
 
-    gridSizer.addEventListener('mouseout', (event) => {
+    resizeBtn.addEventListener('mouseout', (event) => {
 
     event.target.style.color = "red";
         event.target.style.backgroundColor = "black";
     }
     )
 
-  
-            for (y = 1; y <= 16; y++){   
 
-                             
-          
+
+let gridDialog = document.createElement('dialog');
+    gridDialog.setAttribute('id','gridSizeDialog');
+    gridDialog.style.display = 'flex';
+    gridDialog.className = 'gridDialog';
+    gridDialog.style.flexDirection = 'column';
+    gridDialog.style.alignContent = 'center';
+    gridDialog.style.justifyContent = 'stretch';
+    gridDialog.style.border = 'none';  
+    gridDialog.style.cssText = '#gridSizeDialog::backdrop { background-color: red; border-radius: 15px;  }';
+
+
+let dialogForm = document.createElement('form');
+    dialogForm.setAttribute('id','user-grid-size-input')
+    dialogForm.setAttribute('method','dialog');
+    dialogForm.style.display = 'flex';
+    dialogForm.style.flexDirection = 'column';
+    dialogForm.style.width = '100%';
+    dialogForm.style.alignContent = 'center';
+    dialogForm.style.border = '2px';
+    dialogForm.style.borderStyle = 'solid';
+    dialogForm.style.borderRadius = '15px';
+    dialogForm.style.padding = '10px';
+    dialogForm.style.backgroundColor = 'red';
+
+let textArea = document.createElement('div');
+    textArea.style.display = 'flex';
+    textArea.style.flexDirection = 'column';
+    textArea.style.alignContent = 'center';
+
+let inputLabel = document.createElement('label');
+    inputLabel.innerText = 'Enter a number between 10 & 100 to change grid size: \n\n';
+    inputLabel.style.fontWeight = '700';
+
+let formInput = document.createElement('input');
+    formInput.setAttribute('type','number');
+    formInput.setAttribute('min','10');
+    formInput.setAttribute('max','100');
+    formInput.style.height = '150px';
+    formInput.style.fontSize = '100px';
+    formInput.style.backgroundColor = 'lightgray';
+    formInput.style.textAlign = 'center';
+
+let buttonDiv = document.createElement('div');
+    buttonDiv.style.justifyContent = 'space-between';
+    buttonDiv.style.display = 'flex';
+    buttonDiv.style.flexDirection = 'row';
+    
+let inputSubmit = document.createElement('button');
+    inputSubmit.setAttribute('type','submit');
+    inputSubmit.setAttribute('value','16');  //// Value set to default grid size 
+    inputSubmit.innerText = "Submit";
+    inputSubmit.setAttribute('id','inputSubmit');
+    inputSubmit.style.marginTop = '10px';
+    inputSubmit.style.fontWeight = '700';
+    inputSubmit.style.borderRadius = '100%';
+    inputSubmit.style.height = '60px';
 
     
-                const rowDivs = document.createElement('div');
+let formCancel = document.createElement('button');
+    formCancel.setAttribute('type','submit');
+    formCancel.setAttribute('value','cancel');
+    formCancel.setAttribute('form-method','dialog');
+    formCancel.setAttribute('novalidate','true');
+    formCancel.innerText = 'Cancel';
+    formCancel.style.marginTop = '10px';
+    formCancel.style.fontWeight = '700';
+    formCancel.style.borderRadius = '100%';
+    formCancel.style.height = '60px';
+
+    
+textArea.appendChild(inputLabel);
+textArea.appendChild(formInput);
+dialogForm.appendChild(textArea);
+
+buttonDiv.appendChild(inputSubmit);
+buttonDiv.appendChild(formCancel);
+dialogForm.appendChild(buttonDiv);
+
+gridDialog.appendChild(dialogForm);
+divContainer.appendChild(gridDialog);
+
+
+
+
+btnCont.addEventListener('click', () => {
+
+    gridDialog.showModal();
+});
+
+formInput.addEventListener('change', (e) => {
+    inputSubmit.value = formInput.value;
+
+});
+
+resetBtn.addEventListener('click',() => {
+                    
+    divContainer.innerHTML = "";
+
+    createGrid();
+
+})
+
+
+inputSubmit.addEventListener('click', () => { // Empties the outside div by defining its innerHTML as empty
+    divContainer.innerHTML = "";
+    createGrid();
+})      
+
+
+
+                        
+function createGrid(){
+
+for (y = 1; y <= inputSubmit.value; y++)
+            {  
+                 const rowDivs = document.createElement('div');
             
-                    rowDivs.className = "row"+ `${y}`;
+                    rowDivs.className = `${y}`;
             
                     rowDivs.style.clear = "both";
             
                     rowDivs.style.flex = "1 1 0";
             
                     rowDivs.style.display = "flex";
+                    
+                    rowDivs.style.float = 'left';
+                                 
             
-            
-                for (x = 1; x <= 16; x++){
+                for (x = 1; x <= inputSubmit.value; x++){
             
                 const gridDivs = document.createElement('div');
             
-                    gridDivs.className = "grid";
+                    gridDivs.className = "gridDivs";
+
+            gridDivs.style.backgroundColor = 'lightgray'; //setting original background color
+
+           
+
             
+
             
-                
-                rowDivs.appendChild(gridDivs);
+
+const randomRgbColor = () => {
+                let r = Math.floor(Math.random() * 256);
+                let g = Math.floor(Math.random() * 256);
+                let b = Math.floor(Math.random() * 256);
+                        return 'rgb('+r+','+g+','+b+')';
+            }
             
-                    gridDivs.style.backgroundColor = "white"; //setting original background color
+                gridDivs.addEventListener('mouseover',(event) => {
+
+                        let drawColor = gridDivs.style.backgroundColor;
+
+                        if (drawColor == 'lightgray'){
+                            gridDivs.style.backgroundColor = randomRgbColor();
+                        }
+                    })
+
+                  
+  
+        rowDivs.appendChild(gridDivs);
+
+    
             
+            if (y < inputSubmit.value){
             
-            gridDivs.addEventListener('mouseover', function(){ //function to add color change on mouseover
-                const orgColor = gridDivs.style.backgroundColor;
-            
-                if (orgColor === 'white'){
-                    gridDivs.style.backgroundColor = 'black';
-                }
-            });
-            
-            
-            if (y < 16){
-            
-                if (x < 16){     
+                if (x < inputSubmit.value){     
             
                     gridDivs.style.flexGrow = "1";
                     gridDivs.style.borderBottom = '2px';
@@ -120,7 +307,7 @@ gridSizer.addEventListener('mouseover', function(){
             
             } else {
             
-                if (x < 16){     
+                if (x < inputSubmit.value){     
             
                     gridDivs.style.flex = "1 1 0"; 
                     gridDivs.style.borderRight = '2px';
@@ -133,17 +320,55 @@ gridSizer.addEventListener('mouseover', function(){
                                         // special borders for divs that will be on ends so that they don't double with container border
                     gridDivs.style.flex = "1 1 0";  
                     gridDivs.style.float = "left";
+                    
             
-                } 
+                }
+
             
-                   
+}               
             
-            
+
                 
-                 } 
-         
-        } document.getElementById('container').appendChild(rowDivs);
-    }
+                 }   divContainer.appendChild(rowDivs);
+                 
+                 
+                
+                 
+                 
+
+                }
+            }
+        
+
+          
+
+            
+                 
+
+                  
+            
+            
+
+                
+                    
+                
+
+            
+
+
+
+
+                  
+                
+    
+
+
+
+
+
+                
+
+
         
         
              
